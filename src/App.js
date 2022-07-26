1e167f79fea2a1513a33f349287c889966779610
Victor Lawrence <viclawrence99@gmail.com>
2022-07-26 10:06:09 +0100

    Pascal case issue resolve

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import Roadmappage from './Roadmap-page';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Navbar from './Navbar'
-import Two_two from "./two_two";
-import Two_three from "./two_three";
-import Two_four from "./two_four";
-import Two_five from "./two_five";
+import Two from "./two_two";
+import Three from "./two_three";
+import Four from "./two_four";
+import Five from "./two_five";
 
 function App() {
   return (
@@ -19,11 +19,11 @@ function App() {
           <Route path="/*" element={<Home />} />
          <Route path="about" element={<About />} />
          <Route path="roadmap/" element={<Roadmappage />} >
-           <Route index element={<Two_two />} />
-         <Route path="2022" element={<Two_two/>} />
-                  <Route path="2023" element={<Two_three />} />
-                  <Route path="2024" element={<Two_four />} />
-                  <Route path="2025" element={<Two_five />} />
+           <Route index element={<Two />} />
+         <Route path="2022" element={<Two/>} />
+                  <Route path="2023" element={<Three />} />
+                  <Route path="2024" element={<Four />} />
+                  <Route path="2025" element={<Five />} />
          </Route>
         </Routes>
        </div>
